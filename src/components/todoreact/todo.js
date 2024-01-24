dd3a1a2aea90fc7786aa6ffd269c62b886e38515
Safdar-Ali-India <safdarali63166@gmail.com>
2024-01-25 03:44:27 +0530

    checklist done ➕

--- src/components/todoreact/todo.js
+++ src/components/todoreact/todo.js
@@ -9,19 +9,24 @@ const Todo = () => {
   <figure>
     <img src="https://supabase.com/_next/image?url=%2Fimages%2Findex%2Fproducts%2Fvector.svg&w=3840&q=100"  style={{ width: '400px', height: '200px' }}/>
   <figcaption>Add Your List Here 👌</figcaption>
-  
   </figure>
 
   <div className="addItems">
-<input type="text" placeholder='Please Add ➕ Your Items Here' className='form-control' />
+<input type="text" placeholder='Please Add ➕ Your Items Here' 
+className='form-control' />
+<i class="fa fa-solid fa-plus"></i> 
+  </div>
 
+  <div className="showItems">
+
+<button className="btn effect04" data-sm-link-text="Remove All">
+  
+  
+  <span>CHECK LIST</span></button>
 
   </div>
 </div>
-
-
-
-   </div>
+</div>
    </>
   )
 }
